e5e8b884cf3508c04f971de42e6186dac0915aa1
yuta asakura <yuta.asakura.se@gmail.com>
2024-08-09 11:08:15 +0900

    created and styled sticky navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import "@popperjs/core"
+
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 0) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove('sticky')
+  }
+})
